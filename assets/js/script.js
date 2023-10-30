963bb6132549acb4600e362fa680d1aa82e53a20
Blue-Press <Blue-Press@users.noreply.github.com>
2023-10-29 19:53:26 -0600

    password confirmation fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 const registerForm = document.getElementById('registerForm');
 
-registerForm.addEventListener('submit', function () {
+registerForm.addEventListener('submit', function (event) {
+
     let firstPassword = document.getElementById('passwordInput').value;
     let confirmPassword = document.getElementById('confirmPasswordInput').value;
     if (firstPassword === confirmPassword) {
         alert('Registration Complete!')
     } else {
+        event.preventDefault();
         alert('Passwords must match')
+        document.getElementById('confirmPasswordInput').value = '';
+        document.getElementById('confirmPasswordInput').focus();
     }
 
 })
